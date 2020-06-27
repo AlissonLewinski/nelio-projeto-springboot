781f2ab2eb4660d81079b04d2cdb3c99a8d181ae
Alisson Lewinski <lucasali7@gmail.com>
2020-06-27 18:07:44 -0300

    Project created

---
+++ src/main/java/com/alissonlewinski/projetowebservices/ProjetoWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.alissonlewinski.projetowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissonlewinski/projetowebservices/ProjetoWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissonlewinski.projetowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
